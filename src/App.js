2026b0652944abdffd74de9992c07209d32659bc
Iván Gk <ivanstng@gmail.com>
2021-07-07 14:58:46 -0300

    add first iteration of notes circle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,109 @@ import React from 'react';
 import './App.scss';
 
 
+class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      american: props.american,
+    };
+  }
+  static getDerivedStateFromProps(props, state) {
+    return { 
+      name: props.name,
+      american: props.american,
+    };  
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.rotate}> { this.state.american } </button>
+      </div>
+    );
+  }
+}
+
+
+class NotesCircle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          name: ['do'],
+          american: ['C'],
+        }, {
+          name: ['do#', 'reb'],
+          american: ['do#', 'Db'],
+        }, {
+          name: ['re'],
+          american: ['D'],
+        }, {
+          name: ['re#', 'mib'],
+          american: ['D#', 'Eb'],
+        }, {
+          name: ['mi'],
+          american: ['E'],
+        }, {
+          name: ['fa'],
+          american: ['F'],
+        }, {
+          name: ['fa#', 'solb'],
+          american: ['F#', 'Gb'],
+        }, {
+          name: ['sol'],
+          american: ['G'],
+        }, {
+          name: ['sol#', 'lab'],
+          american: ['G#', 'Ab'],
+        }, {
+          name: ['la'],
+          american: ['A'],
+        }, {
+          name: ['la#', 'sib'],
+          american: ['A#', 'Bb'],
+        }, {
+          name: ['si'],
+          american: ['B'],
+        }
+      ],
+    };
+  }
+
+  handleClick(noteClicked) {
+    if (noteClicked.index === 0) {
+      return;
+    }
+    let firstPart = this.state.notes.slice(noteClicked.index);
+    let secondPart = this.state.notes.slice(0, noteClicked.index);
+    let newNotes = firstPart.concat(secondPart);
+    this.setState({
+      notes: newNotes,
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div className="circle-container">
+          <ul className="circle-notes">
+            { this.state.notes.map((note, index) => {
+              return (
+                <li key={index}>
+                  <Note name={note.name} american={note.american} rotate={() => {this.handleClick({...note, index: index})}}/>
+                </li>
+              );
+            }) }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Mode extends React.Component {
   constructor(props) {
     super(props);
@@ -96,7 +199,7 @@ class ModesCircle extends React.Component {
 
     return (
       <div>
-        <div className="circleContainer">
+        <div className="circle-container">
           <ul className="circle">
             { this.state.modes.map((mode, index) => {
               return this.renderMode(mode, index);
@@ -115,6 +218,7 @@ export class App extends React.Component {
     return (
       <div>
         <ModesCircle />
+        <NotesCircle />
       </div>
     );
   }
