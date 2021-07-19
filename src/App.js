69631f0ebe391749dfdf5de09a91ecedfa63665f
Iván Gk <ivanstng@gmail.com>
2021-07-19 11:37:58 -0300

    refactor music circle from app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import './App.scss';
-import { ModesCircle } from './components/modes.js'
-import { NotesCircle } from './components/notes.js'
-import { classicModes } from './variables/scales.js'
+import MusicCircle from './components/music_circle.js'
 
 
 export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentMode: 'mayor (ionian)',
-      currentNote: ['C'],
-    };
-  }
-
-  changeMode(modeName) {
-    this.setState({
-      currentMode: modeName,
-    });
-  }
-
-  changeNote(note) {
-    this.setState({
-      currentNote: note,
-    });
-  }
 
   render() {
     return (
-        <div className='music-circle-section'>
-          <div className='circle-container'>
-            <ModesCircle className='modes-circle-component' modes={classicModes} changeMode={(modeName) => {this.changeMode(modeName)}}/>
-            <div className='inner-circle'>
-              <NotesCircle className='notes-circle-component' changeNote={(note) => {this.changeNote(note)}} />
-            </div>
-          </div>
-          <div className='music-circle-text'>
-            <p> { this.state.currentMode } { this.state.currentNote.join('/') }</p>
-          </div>
-        </div>
+        < MusicCircle />
     );
   }
 }
