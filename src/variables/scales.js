21f606491d2bfc65c015b04e8b51b99f5cf5ac1b
Iván Gk <ivanstng@gmail.com>
2021-07-19 12:15:32 -0300

    add intervals to each mode

--- src/variables/scales.js
+++ src/variables/scales.js
@@ -3,34 +3,41 @@ export const classicModes = [
           tone: 'mayor',
           name: 'ionian',
           color: '#FFAF3B',
+          intervals: [true, false, true, false, true, true, false, true, false, true, false, true],
         }, {
         }, {
           tone: null,
           name: 'dorian',
           color: '#7F9FD0',
+          intervals: [true, false, true, true, false, true, false, true, false, true, true, false],
         }, {
         }, {
           tone: null,
           name: 'phrygian',
           color: '#ACD0EA',
+          intervals: [true, true, false, true, false, true, false, true, false, true, true, false],
         }, {
           tone: null,
           name: 'lydian',
           color: '#FFFFA6',
+          intervals: [true, false, true, false, true, false, true, true, false, true, false, true],
         }, {
         }, {
           tone: null,
           name: 'mixolydian',
           color: '#FF6A4D',
+          intervals: [true, false, true, false, true, true, false, true, false, true, true, false],
         }, {
         }, {
           tone: 'minor',
           name: 'aeolian',
           color: '#6761AB',
+          intervals: [true, false, true, true, false, true, false, true, true, false, true, false],
         }, {
         }, {
           tone: null,
           name: 'locrian',
           color: '#FEC5E8',
+          intervals: [true, true, false, true, false, true, true, false, true, false, true, false],
         }
       ]
