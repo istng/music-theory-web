64658d078e86088c7d06dc31377aa5d223f1162a
Iván Gk <ivanstng@gmail.com>
2021-07-27 10:58:58 -0300

    change layout of desktop to row

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/app.scss';
 import MusicCircle from './components/music_circle.js'
 import ModesTable from './components/modes_table.js'
 
@@ -7,7 +8,7 @@ export class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="app-music-tools">
         <MusicCircle />
         <ModesTable />
       </div>
